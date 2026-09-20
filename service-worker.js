935f4ab48ca16c5a4bf7700ba2e80adbe100c0bd
waiwai <147108916+Yuge-U@users.noreply.github.com>
2026-09-20 21:01:22 +0900

    Disable service worker caching

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const CACHE="screen-shot-v181";
-const ASSETS=["./","./index.html","./app.js?v=1.8.1","./manifest.json"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
+// Version 1.8.2: offline caching disabled so the app always loads the latest files.
+self.addEventListener("install",()=>self.skipWaiting());
+self.addEventListener("activate",event=>{
+event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.registration.unregister()).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",()=>{});
